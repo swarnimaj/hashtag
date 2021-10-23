0383b70e97a6017205d0ba63cbc03e1cef7a1030
swarnimaj <65511954+swarnimaj@users.noreply.github.com>
2021-10-23 10:46:11 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+const rootComponent = {
+  data: ()=> {
+    return{
+      text: '',
+      results: []
+    }
+  },
+  methods: {
+    updateResults: function(query){
+      query=encodeURIComponent(query);
+      let url = `https://www.google.com/complete/search?client=firefox&q=${query}`;
+      fetch(url).then(response=>{
+        return response.json();
+      }).then(response=>{
+        let results = response[1];
+        let fixedResults = [];
+        for(var i=0; i<results.length; i++){
+          let hashtag = results[i].split(' ')[0];
+          if(hashtag.startsWith('#') && !fixedResults.includes(hashtag)){
+            fixedResults.push(hashtag);
+          }
+        }
+        this.results = fixedResults;
+      })
+    },
+    autoComplete: function(){
+      if(this.text.startsWith('#')){
+        this.updateResults(this.text);
+      }
+    },
+    setQuery: function(query){
+      this.text = query;
+    }
+
+  },
+  template: `
+  <div class="center">
+    <input type="text" @input="autoComplete" v-model="text" >
+    <p v-for="result in results">
+    <span @click="setQuery(result)">{{result}}</span>
+    </p>
+  </div>
+
+  `,
+}
+
+const vm = Vue.createApp(rootComponent).mount('#app');
